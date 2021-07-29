fab5a5a9a82bf593c15319cc65bc9d946a67901b
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-07-29 13:30:28 +0200

    feat: on click show id

--- src/playlist.js
+++ src/playlist.js
@@ -4,23 +4,37 @@ import "./playlist.css";
 import { getMediaList } from "./api";
 import { OneMovieComponent } from "./oneMovieComponent";
 
-const bodyMedia = { MediaListId: 2, PageNumber: 1, PageSize: 15 };
-
 export default function Playlist({ goBack, jwtToken }) {
 	const [mediaList, setMediaList] = useState("Loading Media List...");
 	const [dataIsLoaded, setDataIsLoaded] = useState(false);
+
+	// TODO input user, media list id
+	const [mediaListId, setMediaListId] = useState(3);
+
+	const bodyMedia = { MediaListId: mediaListId, PageNumber: 1, PageSize: 15 };
 
 	function getMedia(bodyMedia, jwtToken) {
 		const dataMedia = getMediaList(bodyMedia, jwtToken);
 		return dataMedia;
 	}
 
+	function onClickLog(e, id) {
+		console.log(id);
+	}
+
 	function RenderMovieDetails(isLoaded) {
 		if (isLoaded) {
-			let howManyMovies = mediaList.Entities.length;
-			let value = mediaList.Entities[0].Title;
 			return Object.entries(mediaList.Entities).map((el) => {
-				return <div className="oneMovie">{el[1].Title}</div>;
+				return (
+					<div
+						className="oneMovie"
+						onClick={(e) => onClickLog(e, el[1].Id)}
+					>
+						{el[1].Title}
+						<br />
+						{el[1].Id}
+					</div>
+				);
 			});
 		} else {
 			return 0;
